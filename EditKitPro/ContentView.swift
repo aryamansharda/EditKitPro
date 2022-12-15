3e57684f1a30500ab8f5ba935be9300d8a87cfc6
aryamansharda <asharda@turo.com>
2022-12-14 21:53:24 -0800

    Initial Commit

---
+++ EditKitPro/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  EditKitPro
+//
+//  Created by Aryaman Sharda on 12/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EditKitPro/EditKitProApp.swift
@@ -0,0 +1,17 @@
+//
+//  EditKitProApp.swift
+//  EditKitPro
+//
+//  Created by Aryaman Sharda on 12/14/22.
+//
+
+import SwiftUI
+
+@main
+struct EditKitProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
